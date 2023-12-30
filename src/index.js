5e1fbfc0631f00e6941d865f5adc40c5e3791ddc
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-12-30 20:42:54 +0100

    index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {useLoadingStore} from "./store/loading";
+import Icon from "./components/icon.vue";
+import loading from "./components/Loading.vue";
+import {DialogComponent} from "./contracts/DialogComponent";
+import {PixlEntry, EntryMeta} from "./contracts/PixlEntry";
+import Dialog from "./components/dialog.vue";
+
+export {
+    useLoadingStore,
+    Icon,
+    loading,
+    Dialog,
+    DialogComponent,
+    PixlEntry,
+    EntryMeta,
+}
