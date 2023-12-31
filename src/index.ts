7a1ef15df1189133835ad12a18b92d5ee9791638
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-12-31 11:05:28 +0100

    fixed type exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import {useLoadingStore} from "./store/loading";
 import {useDialogStore} from "./store/dialog";
 import Icon from "./components/icon.vue";
-import loading from "./components/Loading.vue";
-import {DialogComponent} from "./contracts/DialogComponent";
-import {PixlEntry, EntryMeta} from "./contracts/PixlEntry";
+import Loading from "./components/Loading.vue";
 import Dialog from "./components/dialog.vue";
 
 export {
     useLoadingStore,
     useDialogStore,
     Icon,
-    loading as Loading,
+    Loading,
     Dialog,
-    DialogComponent,
-    PixlEntry,
-    EntryMeta,
 }
+
+export type {PixlEntry, EntryMeta} from "./contracts/PixlEntry";
+export type {DialogComponent} from "./contracts/DialogComponent";
