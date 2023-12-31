f11d252cc68940f500d0196062befa7450be40c0
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-12-31 09:58:35 +0100

    additional export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {useLoadingStore} from "./store/loading";
+import {useDialogStore} from "./store/dialog";
 import Icon from "./components/icon.vue";
 import loading from "./components/Loading.vue";
 import {DialogComponent} from "./contracts/DialogComponent";
@@ -7,6 +8,7 @@ import Dialog from "./components/dialog.vue";
 
 export {
     useLoadingStore,
+    useDialogStore,
     Icon,
     loading as Loading,
     Dialog,
